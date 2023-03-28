8b144ba0f58b740d4e78d191c76c9d61b2f9d8a4
nvnamsss <nvnam.c@gmail.com>
2023-03-28 10:59:12 +0700

    Update katana abi

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package bridge_contracts
 import (
 	ethereumGateway "github.com/axieinfinity/bridge-contracts/generated_contracts/ethereum/gateway"
 	ethereumGovernance "github.com/axieinfinity/bridge-contracts/generated_contracts/ethereum/governance"
+	"github.com/axieinfinity/bridge-contracts/generated_contracts/katana"
 	bridgeTracking "github.com/axieinfinity/bridge-contracts/generated_contracts/ronin/bridge_tracking"
 	roninGateway "github.com/axieinfinity/bridge-contracts/generated_contracts/ronin/gateway"
 	roninGovernance "github.com/axieinfinity/bridge-contracts/generated_contracts/ronin/governance"
@@ -29,4 +30,5 @@ var ABIMaps = map[string]*bind.MetaData{
 	"BridgeTracking": bridgeTracking.BridgeTrackingMetaData,
 	"Validator":      validator.ValidatorMetaData,
 	"Staking":        ronStaking.RonStakingMetaData,
+	"Katana":         katana.KatanaMetaData,
 }
